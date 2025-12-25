dfc103f32f5ec44cadb34f0006d363ba3d8a0adc
chandra kumar <150223034+chandrakumar301@users.noreply.github.com>
2025-12-25 17:32:05 +0530

    added pen to canva and make sign

--- src/Sign1.jsx
+++ src/Sign1.jsx
@@ -1,45 +1,48 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 
-function SignCanvas(Sign2) {
-  const canvasRef = useRef(null);
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    let offsetX = 0;
-    let offsetY = 0;
-    let isDragging = false;
-    const mouseDown = (e) => {
-      isDragging = true;
-      offsetX = e.clientX - canvas.offsetLeft;
-      offsetY = e.clientY - canvas.offsetTop;
-    };
-    const mouseMove = (e) => {
-      if (!isDragging) return;
-      canvas.style.left = e.clientX - offsetX + "px";
-      canvas.style.top = e.clientY - offsetY + "px";
-    };
-    const mouseUp = () => {
-      isDragging = false;
-    };
-    canvas.addEventListener("mousedown", mouseDown);
-    document.addEventListener("mousemove", mouseMove);
-    document.addEventListener("mouseup", mouseUp);
-    return () => {
-      canvas.removeEventListener("mousedown", mouseDown);
-      document.removeEventListener("mousemove", mouseMove);
-      document.removeEventListener("mouseup", mouseUp);
-    };
-  }, [Sign2]);
+// import { useRef } from "react";
+
+function SignCanvas() {
+  const abc=useRef(false);
+  const canvas=useRef(null);
+  const Down = () => {
+    abc.current=true;
+  } 
+  const Move = (e) => {
+    if(abc.current){
+    const rect=canvas.current.getBoundingClientRect();
+    const ctx=canvas.current.getContext("2d");
+    const x=e.clientX-rect.left;
+    const y=e.clientY-rect.top;
+    ctx.lineWidth=2;
+    ctx.strokeStyle="white";
+    ctx.lineCap="round";
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    }
+    else{
+      return;
+    }
+  }
+  const Up = () => {
+    abc.current=false;
+  }
 
   return (
     <canvas
-      ref={canvasRef}
       className="sign_input"
       width={300}
+      ref={canvas}
       height={150}
+      onMouseDown={Down}
+      onMouseMove={Move}
+      onMouseUp={Up}
       style={{
         position: "absolute",
         border: "1px solid black",
-        cursor: "grab",
+        cursor: "inherit",
         backgroundColor: "black",
       }}
     />
--- src/Text1.jsx
+++ src/Text1.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import "./App.css";
+//import "./App.css";
 
 function Text({setInputValue,inputValue ,TextValue}) {
 
